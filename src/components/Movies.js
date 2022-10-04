e95783cd0a0f755ce99a1534957799f84e4884a5
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-10-04 10:30:24 +0300

    Pass 13 fail 10

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const items = movies.map((movie)=> (
+    <div key={movie/title}>
+
+      <h2>{movie.title}</h2>
+      <p>Runtime:{movie.time} min.</p>
+      <ul>
+        {movie.genres.map((genre)=>(
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      {items}
+    </div>
+  )
+  
+  
 }
 
 export default Movies;
